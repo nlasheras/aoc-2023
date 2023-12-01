d6f036404aaf1ceae5e9f86b5b42c378ffa0d2cc
Nacho Lasheras <nacho@orcsandfireballs.com>
2023-12-01 14:32:55 +0200

    Solve second star for day 1

--- src/day01.rs
+++ src/day01.rs
@@ -10,46 +10,57 @@ pub fn parse_input(input: &str) -> Vec<String> {
     ret
 }
 
-pub fn find_first_digit(input: &String) -> u32 {
-    for c in input.chars() {
+pub fn get_digits(input: &String, also_text: bool) -> Vec<u32> {
+    let nums = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let mut ret = Vec::<u32>::new();
+    for i in 0..input.len() {
+        let c = input.chars().nth(i).unwrap();
         if c.is_numeric() {
-            return c.to_digit(10).unwrap()
+            ret.push(c.to_digit(10).unwrap())
+        }
+        if also_text {
+            let sub = &input[i..input.len()];
+            for j in 0..10 {
+                if sub.starts_with(nums[j]) {
+                    ret.push(j as u32);
+                    break;
+                }
+            }
         }
     }
-    0
+    ret
 }
 
-pub fn find_last_digit(input: &String) -> u32 {
-    for c in input.chars().rev() {
-        if c.is_numeric() {
-            return c.to_digit(10).unwrap()
-        }
-    }
-    0
-}
-
-pub fn find_calibration_value(input: &String) -> u64 {
-    (find_first_digit(input) * 10 + find_last_digit(input)) as u64
+pub fn find_calibration_value(input: &String, also_text: bool) -> u64 {
+    let digits = get_digits(input, also_text);
+    let first = *digits.first().unwrap();
+    let last = *digits.last().unwrap();
+    (first * 10 + last) as u64
 }
 
 #[aoc(day1, part1)]
 pub fn solve_part1(entries: &[String]) -> u64 {
-    let mut sum = 0;
-    for line in entries {
-        sum += find_calibration_value(line)
-    }
-    sum
+    entries.iter().fold(0, |sum, line| sum + find_calibration_value(line, false))
 }
+
+#[aoc(day1, part2)]
+pub fn solve_part2(entries: &[String]) -> u64 {
+    entries.iter().fold(0, |sum, line| sum + find_calibration_value(line, true))
+} 
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_day1_string3() {
+    fn test_day1_part1_example3() {
         let input = String::from("a1b2c3d4e5f");
-        assert_eq!(find_first_digit(&input), 1);
-        assert_eq!(find_last_digit(&input), 5);
-        assert_eq!(find_calibration_value(&input), 15);
+        assert_eq!(find_calibration_value(&input, false), 15);
+    }
+
+    #[test]
+    fn test_day1_part2_example7() {
+        let input = String::from("7pqrstsixteen");
+        assert_eq!(find_calibration_value(&input, true), 76);
     }
 }
