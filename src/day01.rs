fda8243ae70cbc43f01849ee0492d0cf4609cc53
Nacho Lasheras <fanatiko@gmail.com>
2023-12-01 18:45:59 +0200

    Small refactor to handle the text values better

--- src/day01.rs
+++ src/day01.rs
@@ -10,23 +10,27 @@ pub fn parse_input(input: &str) -> Vec<String> {
     ret
 }
 
-pub fn get_digits(input: &String, also_text: bool) -> Vec<u32> {
-    let nums = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+pub fn get_digits(input: &str, also_text: bool) -> Vec<u32> {
+    let nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     let mut ret = Vec::<u32>::new();
-    for i in 0..input.len() {
+    let mut i = 0;
+    while i < input.len() {
         let c = input.chars().nth(i).unwrap();
+        let mut read = 1;
         if c.is_numeric() {
             ret.push(c.to_digit(10).unwrap())
-        }
-        if also_text {
+        } else if also_text {
             let sub = &input[i..input.len()];
             for j in 0..10 {
-                if sub.starts_with(nums[j]) {
+                let as_word = nums[j];
+                if sub.starts_with(as_word) {
                     ret.push(j as u32);
+                    read = as_word.len();
                     break;
                 }
             }
         }
+        i += read;
     }
     ret
 }
@@ -62,5 +66,11 @@ mod tests {
     fn test_day1_part2_example7() {
         let input = String::from("7pqrstsixteen");
         assert_eq!(find_calibration_value(&input, true), 76);
+    }
+
+    #[test]
+    fn test_day1_part2_refactor() {
+        let input = String::from("7pqrstsix7teen");
+        assert_eq!(find_calibration_value(&input, true), 77);
     }
 }
