9135ab35c6201b79e7f02777b8c5fe94520932a0
Nacho Lasheras <fanatiko@gmail.com>
2023-12-19 20:10:37 +0200

    Parsing for Day 17

--- README.md
+++ README.md
@@ -60,3 +60,8 @@ Part one was pretty fast to do. Part two was also pretty fast to do, I was a bit
 ### Day 16 :warning:
 
 Today the biggest problem I got was in Part 1, since even with the example my starting implementation was staying on a loop. My starting solution for that was just splitting the beams first time a beam passes over there, which ensures that the algorithm will end eventually. I tried a more correct version of the algorithm afterwards, but my first implementation was much slower.
+
+### Day 17 :stop_sign: 
+
+I got stuck a bit with this problem for a while. I have tried my implementations of A* and Dijkstra but seems that there is something I'm missing on the puzzle. I'll go ahead for now and work on this during the holidays.
+
---
+++ src/day17.rs
@@ -0,0 +1,79 @@
+use aoc_runner_derive::aoc;
+use aoc_runner_derive::aoc_generator;
+
+use crate::utils::Grid;
+
+use priority_queue::DoublePriorityQueue;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+
+#[aoc_generator(day17)]
+pub fn parse_input(input: &str) -> Grid<usize>  {
+    Grid::from(input, |c| c.to_string().parse::<usize>().unwrap())
+}
+
+fn heat_loss_path(input: &Grid<usize>, path: &Vec<(i32, i32)>) -> u64 {
+    path.iter().skip(1).map(|(x,y)| input.cell_at(*x, *y).unwrap() as u64).sum::<u64>()
+}
+
+
+fn print_path(input: &Grid<usize>, path: &Vec<(i32, i32)>) {
+    let mut buffer = "".to_string();
+    for y in 0..input.size().1 {
+        for x in 0..input.size().0 {
+            let cell = input.cell_at(x as i32, y as i32).unwrap();
+            if let Some(pos) = path.iter().position(|p| p.0 == x as i32 && p.1 == y as i32) {
+                if pos == 0 {
+                    buffer.push_str(&cell.to_string());
+                } else {
+                    let dir = (path[pos].0 - path[pos-1].0, path[pos].1 - path[pos-1].1);
+                    let char = match dir {
+                        (1, 0) => '>',
+                        (-1, 0) => '<',
+                        (0, 1) => 'v',
+                        (0, -1) => '^',
+                        _ => panic!()
+                    };
+                    buffer.push(char);
+                }
+            } else {
+                buffer.push_str(&cell.to_string());
+            }
+        }
+        buffer.push('\n');
+    }
+    println!("{}", buffer)
+}
+
+
+#[aoc(day17, part1)]
+pub fn sum_heat_loss(input: &Grid<usize>) -> u64 {
+    0
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DAY17_EXAMPLE: &str = "2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533";
+
+    #[test]
+    fn test_day16_part1() {
+        let input = parse_input(DAY17_EXAMPLE);
+        assert_eq!(sum_heat_loss(&input), 102);
+    }
+
+}
