38f786d230caa4abed36b7b8857fd8beaef3e79f
Nacho Lasheras <fanatiko@gmail.com>
2023-12-28 07:55:41 +0100

    Use Point for day 24

--- src/day24.rs
+++ src/day24.rs
@@ -1,10 +1,14 @@
 use aoc_runner_derive::aoc;
 use aoc_runner_derive::aoc_generator;
 
+use crate::utils::Vector;
+
+type Point = Vector<i64>;
+
 #[derive(Clone)]
 pub struct Hailstone {
-    pub position: (i64, i64, i64),
-    pub velocity: (i64, i64, i64)
+    pub position: Point,
+    pub velocity: Point
 }
 
 impl Hailstone {
@@ -12,7 +16,7 @@ impl Hailstone {
         let mut parts = input.split(" @ ");
         let pos : Vec<i64> = parts.next().unwrap().split(", ").map(|s| s.trim().parse::<i64>().unwrap() ).collect();
         let vel : Vec<i64> = parts.next().unwrap().split(", ").map(|s| s.trim().parse::<i64>().unwrap()).collect();
-        Hailstone{ position: (pos[0], pos[1], pos[2]), velocity: (vel[0], vel[1], vel[2]) }
+        Hailstone{ position: Point::new_3d(pos[0], pos[1], pos[2]), velocity: Point::new_3d(vel[0], vel[1], vel[2]) }
     }
 }
 
@@ -22,22 +26,21 @@ pub fn parse_input(input: &str) -> Vec<Hailstone> {
 }
 
 fn find_crossing_2d(a: &Hailstone, b: &Hailstone) -> Option<(f64, f64)> {
-    let dx = b.position.0 - a.position.0;
-    let dy = b.position.1 - a.position.1;
-    let det = b.velocity.0 * a.velocity.1 - b.velocity.1 * a.velocity.0;
+    let dx = b.position.x - a.position.x;
+    let dy = b.position.y - a.position.y;
+    let det = b.velocity.x * a.velocity.y - b.velocity.y * a.velocity.x;
     if det == 0 {
         return None;
     }
-    let u = (dy * b.velocity.0 - dx * b.velocity.1) as f64 / det as f64;
-    let v = (dy * a.velocity.0 - dx * a.velocity.1) as f64 / det as f64;
+    let u = (dy * b.velocity.x - dx * b.velocity.y) as f64 / det as f64;
+    let v = (dy * a.velocity.x - dx * a.velocity.y) as f64 / det as f64;
     if u < 0.0 || v < 0.0 {
         return None; // cross in the past
     }
-    Some((a.position.0 as f64 + u*(a.velocity.0 as f64), a.position.1 as f64 + u*(a.velocity.1 as f64)))
+    Some((a.position.x as f64 + u*(a.velocity.x as f64), a.position.y as f64 + u*(a.velocity.y as f64)))
 }
 
 pub fn count_crossing(entries: &[Hailstone], min: i64, max: i64) -> u64 {
-
     let mut count = 0;
     for i in 0..entries.len() {
         for j in i+1..entries.len() {
@@ -58,6 +61,26 @@ pub fn count_crossing_part1(entries: &[Hailstone]) -> u64 {
     count_crossing(entries, 200000000000000, 400000000000000)
 }
 
+#[allow(dead_code)]
+fn find_crossing_3d(a: &Hailstone, b: &Hailstone) -> Option<(f64, f64, f64)> {
+    let da = a.velocity;
+	let db = b.velocity;
+	let dc = b.position - a.position;
+	
+    let div = da.cross(&db).length_sq() as f64;
+    if div == 0.0 {
+        return None;
+    }
+	let s = dc.cross(&db).dot(&da.cross(&db)) as f64 / div;
+	if s >= 0.0 {
+        let x = a.position.x as f64 + a.velocity.x as f64 * s;
+        let y = a.position.y as f64 + a.velocity.y as f64 * s;
+        let z = a.position.z as f64 + a.velocity.z as f64 * s;
+		return Some((x, y, z))
+	}
+	None
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -74,4 +97,10 @@ mod tests {
         assert_eq!(count_crossing(&input, 7, 27), 2);
     }
 
+    #[test]
+    fn test_day24_crossing3d() {
+        let h0 = Hailstone::from("19, 13, 30 @ -2,  1, -2");
+        let res = Hailstone::from("24, 13, 10 @ -3, 1, 2");
+        assert_eq!(find_crossing_3d(&h0, &res), Some((9f64, 18f64, 20f64)));
+    }
 }
